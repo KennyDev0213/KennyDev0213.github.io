08f1d61b488f95623692328fc4f9b3216e527774
KennyDev0213 <58504944+KennyDev0213@users.noreply.github.com>
2022-12-03 17:50:20 -0800

    reimplemented showcase page for later use

---
+++ src/modules/Views/showcasePage.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+function ShowcasePage(){
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default ShowcasePage;
