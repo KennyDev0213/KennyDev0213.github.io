1301f2439ac54a1bb80f3dbe14904c84da163d04
KennyDev0213 <58504944+KennyDev0213@users.noreply.github.com>
2023-01-18 13:09:53 -0800

    Added 'LinkedIn' to contacts

--- src/modules/Views/ContactPage.jsx
+++ src/modules/Views/ContactPage.jsx
@@ -7,6 +7,7 @@ const ContactPage = () => {
         <ul>
             <li><h4>Github: - </h4><a href="https://github.com/KennyDev0213">github</a></li>
             <li><h4>Itch.IO: - </h4><a href="https://kennydev0213.itch.io/">itch.io</a></li>
+            <li><h4>LinkedIn: - </h4><a href="https://www.linkedin.com/in/kennon-dong-724843241/">itch.io</a></li>
             <li><h4>Email: - </h4> Kedong0213@gmail.com</li>
             <li><h4>Phone Number: - </h4> 778-861-8003</li>
         </ul>
